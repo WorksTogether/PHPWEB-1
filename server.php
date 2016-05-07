2ae8d859513cf95b80da5d885c3444f5e1e96990
lzq <357881371@qq.com>
2016-05-07 16:20:05 +0800

    修改。。。

--- server.php
+++ server.php
@@ -1,27 +1,13 @@
 <?php
 
-error_reporting(E_ALL);
+error_reporting(E_ERROR);
 set_time_limit(0);
 
 date_default_timezone_set('Europe/London');
-
+include 'connect.php';
 include 'PHPExcel/IOFactory.php';
 
-
-// $inputFileName = './sampleData/example1.xls';
-// $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
-// $sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-// $output["page"] = 1;
-// $output["total"] = 1;
-// $output["records"] = 1;
-// foreach ($sheetData as $key => $value) {
-// 	$output["rows"][$key-1]["id"] = $key-1;
-// 	$output["rows"][$key-1]["cell"] = $value;
-// };
-// $output = '';
-// echo json_encode($output, JSON_UNESCAPED_UNICODE);
 $uploadfile_path = "";
-// $file_name = "";
 $GLOBALS['file_name'] = "";
 $action = $_GET['action']; 
 switch ($action) {
@@ -35,10 +21,54 @@ switch ($action) {
     case 'request_file':
 		request_file($GLOBALS['file_name']);
   break;
+  case 'del_row':
+    del_row();
+  break;
+   case 'check_fin':
+    check_fin();
+  break;
+  case 'first_refresh':
+    first_refresh();
+  break;
   default:
     # code...
     break;
 };
+function  first_refresh(){
+  $sql = "";
+  $sql = "SELECT * FROM `total` WHERE status = 'case_in'";
+  if($result = $GLOBALS['$conn']->query($sql))
+  {
+    if ($result->num_rows > 0)
+     {
+      $output->page = 3;
+      $output->total = 3;
+      $output->records = 50;
+      $i = 0;
+      while($row = $result->fetch_assoc()) 
+      {
+        $responce->rows[$i]['id'] = $row['id'];
+        $responce->rows[$i]['cell'] = array ( 
+                $row['id'],
+                $row['name'], 
+                $row['sex'], 
+                $row['status'], 
+            ); 
+            $i++; 
+      }
+      echo json_encode($responce);
+    }
+    else
+    {
+          $reponse["records"] = 0;
+    echo json_encode($reponse,JSON_UNESCAPED_UNICODE);
+    }
+  }
+  else
+  {
+    echo "Error: " . $sql . "<br>" . $GLOBALS['$conn']->error;
+  }
+}
 function handle_fileupload()
 {
   $dir = "./uploads";
@@ -62,7 +92,7 @@ if ( !empty( $_FILES ) ) {
   		else
    		{
    			$tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
-    		$uploadPath = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_FILES[ 'file' ][ 'name' ];
+        $uploadPath = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . 'case_in.xls';
     		$uploadfile_path = $uploadPath;
     		$GLOBALS['file_name'] = $_files["file"]["name"];
     		move_uploaded_file( $tempPath, $uploadPath );
@@ -79,22 +109,102 @@ if ( !empty( $_FILES ) ) {
 }
 }
 function request_file($filename){
-// $inputFileName = "./uploads/" . $_FILES["file"]["name"];
-$inputFileName = './uploads/example1.xls';
-// $excel_obj = new PHPExcel();
-// $objWriter = new PHPExcel_Writer_Excel5($excel_obj);
+$str = "";
+$sql = "";
+$inputFileName = './uploads/case_in.xls';
 $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
-$objPHPExcel->getActiveSheet()->removeRow(1);
-// $objWriter->save($inputFileName);  
-$sheetData = $objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-$output["page"] = 1;
-$output["total"] = 1;
-$output["records"] = 1;
-foreach ($sheetData as $key => $value) {
-	$output["rows"][$key-1]["id"] = $key-1;
-	$output["rows"][$key-1]["cell"] = $value;
-};
-// $output = '';
-echo json_encode($output, JSON_UNESCAPED_UNICODE);
+$objPHPExcel->getActiveSheet()->removeRow(1); 
+$objWorksheet = $objPHPExcel->getActiveSheet();
+
+$highestRow = $objWorksheet->getHighestRow(); // e.g. 10
+$highestColumn = $objWorksheet->getHighestColumn(); // e.g 'F'
+$highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn); 
+
+for ($row = 1; $row <= $highestRow; ++$row)
+{
+  for ($col = 0; $col <= $highestColumnIndex; ++$col)
+  {
+    $str .= htmlspecialchars(stripslashes(trim($objWorksheet->getCellByColumnAndRow($col, $row)->getValue()))).'\\';
+  }
+  $strs = explode("\\",$str);
+  $sql = "INSERT INTO `total`(`name`, `sex`,`status`) VALUES ('$strs[0]','$strs[1]','case_in')";
+  // $GLOBALS['$conn']->query($sql);
+  if($GLOBALS['$conn']->query($sql))
+  {
+
+  }
+  else {
+    echo "Error: " . $sql . "<br>" . $GLOBALS['$conn']->error;
+}
+  $str = "";
+}
+
+$sql = "SELECT * FROM `total` WHERE status = 'case_in'";
+  if($result = $GLOBALS['$conn']->query($sql))
+  {
+    if ($result->num_rows > 0)
+     {
+      $output->page = 1;
+      $output->total = 1;
+      $output->records = 1;
+      $i = 0;
+      while($row = $result->fetch_assoc()) 
+      {
+        // echo json_encode($row);
+        $responce->rows[$i]['id'] = $row['id'];
+        $responce->rows[$i]['cell'] = array ( 
+                $row['id'],
+                $row['name'], 
+                $row['sex'], 
+                $row['status'], 
+            ); 
+            $i++; 
+      }
+      echo json_encode($responce);
+    }
+  }
+  else
+  {
+    echo "Error: " . $sql . "<br>" . $GLOBALS['$conn']->error;
+  }
+}
+function del_row(){
+  $sql = "";
+  $ids = $_POST['sels'];
+  if (empty ($ids)) 
+  {
+     die("0");
+  }     
+  $ids_str = join(',', $ids); 
+  $sql = "DELETE FROM `total` WHERE id IN ($ids_str)";
+if($GLOBALS['$conn']->query($sql))
+  {
+   echo json_encode("success");
+  }
+  else
+  {
+    echo "Error: " . $sql . "<br>" . $GLOBALS['$conn']->error;
+  }
+
+}
+function check_fin(){
+  $sql = "";
+  $ids = $_POST['sels'];
+  $ids_str = join(',', $ids);
+  if (empty ($ids)) 
+  {
+     die("0");
+  }  
+$sql = "UPDATE `total` SET `status`='wait_assign' WHERE id IN ($ids_str)";
+// $GLOBALS['$conn']->query("SET AUTOCOMMIT=0");
+// $GLOBALS['$conn']->query("BEGIN");
+if($GLOBALS['$conn']->query($sql))
+{
+echo json_encode("success");
+}
+else
+{
+   echo "Error: " . $sql . "<br>" . $GLOBALS['$conn']->error;
+ }
 }
 ?>
