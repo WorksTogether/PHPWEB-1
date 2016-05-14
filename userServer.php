65136c6b2bf20eafb7cf76761d4bc6e2599d6b26
KS <564289319@qq.com>
2016-05-14 21:17:56 +0800

    login

---
+++ userServer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KS
+ * Date: 2016/5/14
+ * Time: 21:09
+ */
+error_reporting(E_ERROR);
+set_time_limit(0);
+
+date_default_timezone_set('Europe/London');
+include 'connect.php';
+$action = $_POST['action'];
+
+?>
